4c13c09b76f78c9c0005646d3e02208af4a88eb7
yernaz-togizbayev <yernaz.togizbayev@gmail.com>
2023-06-03 16:16:44 +0200

    implement MagicLevel enum class

--- src/a01429473/MagicLevel.java
+++ src/a01429473/MagicLevel.java
@@ -1,4 +1,30 @@
 package a01429473;
 
+/**
+ * Defines the various magic levels.
+ * wizards have a magic level.
+ * in order to be able to use specific spells a minimum magic level is necessary.
+ * Note: the compiler generated default constructor may not be sufficient for your implementation
+ */
 public enum MagicLevel {
+	NOOB(50),
+	ADEPT(100),
+	STUDENT(200),
+	EXPERT(500),
+	MASTER(1000);
+	
+	private int MP;
+	
+	MagicLevel(int MP) {
+		this.MP = MP;
+	}
+
+	public int toMana() {
+		return MP;
+	}
+	
+	@Override
+	public String toString() {
+		return "*".repeat(ordinal() + 1);
+	}
 }
