a157254a3e4eed19685e1dbbec90f3486ef9c74f
yernaz-togizbayev <yernaz.togizbayev@gmail.com>
2023-06-03 16:30:08 +0200

    implement HealthPotion class

--- src/a01429473/HealthPotion.java
+++ src/a01429473/HealthPotion.java
@@ -1,17 +1,50 @@
 package a01429473;
 
+/**
+ * HealthPotions increase the consumer's mana
+ */
 public class HealthPotion extends Potion {
-	private int health;  //must not be negative
-
-	@Override
-	public String additionalOutputString() {
-	// returns "; + ’ health ’ HP ";
-	//e.g. ( total result of toString ) "[ Health Potion ; 1 g; 1 Knut ; 5 gulps ; +10 HP ]"
+	
+	private int health; // Must not be negative
+	
+	/**
+	 * @param name name
+	 * @param usages number of usages still left
+	 * @param price price
+	 * @param weight weight
+	 * @param health change of health on consumer
+	 */
+	public HealthPotion(String name, int usages, int price, int weight, int health) {
+		super(name, usages, price, weight);
+		
+		if (health < 0)
+			throw new IllegalArgumentException("health must not be negative");
+		
+		this.health = health;
 	}
 
+	/**
+	 * returns "; +'health' HP".
+	 * e.g. (total result of toString) "[Health Potion; 1 g; 1 Knut; 5 gulps; +10 HP]"
+	 * @return "; +'health' HP"
+	 */
+	@Override
+	public String additionalOutputString() {
+		// returns "; + ’ health ’ HP ";
+		//e.g. ( total result of toString ) "[ Health Potion ; 1 g; 1 Knut ; 5 gulps ; +10 HP ]"
+		return "; +" + health + " HP ";
+	}
+
+	/**
+	 * If usages is greater than 0 reduce usages by 1 (tryUsage method) and
+	 * increase HP of target by health (call method heal(health))
+	 * @param target target on which to object is to be used on
+	 */
 	@Override  
 	public void useOn(MagicEffectRealization target) {
-	//if usages>0 reduce usages by 1 (tryUsage method) and
-	//increase HP of target by health (call method heal(health)
+		//if usages>0 reduce usages by 1 (tryUsage method) and
+		//increase HP of target by health (call method heal(health)
+		if (tryUsage())
+			target.heal(health);
 	}
 }
