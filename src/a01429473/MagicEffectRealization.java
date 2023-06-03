47f0f6d6efde0bbd729d38399329b66c9ded135b
yernaz-togizbayev <yernaz.togizbayev@gmail.com>
2023-06-03 16:23:59 +0200

    add MagicEffectRealization default implementations

--- src/a01429473/MagicEffectRealization.java
+++ src/a01429473/MagicEffectRealization.java
@@ -2,52 +2,178 @@ package a01429473;
 
 import java.util.Set;
 
+/**
+ * This interface contains all the methods to realize various magic effects on an object.
+ * Most methods default to doing nothing and should be overriden for classes that can be
+ * influenced by magic. The overrides define what exactly will happen given a specific
+ * magic effect on the specific object.
+ */
 public interface MagicEffectRealization {
-	default void takeDamage(int amount) {}
-    //negative amount must throw IllegalArgumentException;
-    //a typical implementation will reduce the object's HP by amount ensuring however that HP does not become negative.
 
-	default void takeDamagePercent(int percentage) {}
-    //percentage must be between 0 and 100 (inclusive) otherwise an IllegalArgumentException must be thrown; 
-    //a typical implementation will reduce the object's HP by the percentage given of the object's basic HP value ensuring however, that HP does not become negative.
-    //calculations must be done in double data type converting back to int only in the last step
+	/**
+     * Negative amount must throw IllegalArgumentException;
+     * a typical implementation will reduce the object's HP by amount ensuring however
+     * that HP does not become negative.
+     * 
+     * @param amount amount of damage done
+     */
+	default void takeDamage(int amount) {
+		// negative amount must throw IllegalArgumentException;
+		// a typical implementation will reduce the object's HP by amount ensuring however that HP does not become negative.
+		if (amount < 0)
+			throw new IllegalArgumentException("amount must not be negative");
+	}
+    
+	/**
+     * Percentage must be between 0 and 100 (inclusive) otherwise an IllegalArgumentException
+     * must be thrown;
+     * a typical implementation will reduce the object's HP by the percentage given of the
+     * object's basic HP value ensuring however, that HP does not become negative.
+     * calculations must be done in double data type converting back to int only in the
+     * last step
+     * @param percentage percentage of damage relative to object's basic HP value
+     */
+	default void takeDamagePercent(int percentage) {
+		// percentage must be between 0 and 100 (inclusive) otherwise an IllegalArgumentException must be thrown;
+		// a typical implementation will reduce the object's HP by the percentage given of the object's basic HP value ensuring however, that HP does not become negative.
+		// calculations must be done in double data type converting back to int only in the last step
+		if (percentage < 0 || percentage > 100)
+			throw new IllegalArgumentException("percentage must be between 0 and 100");
+	}
 
-	default void weakenMagic(int amount) {}
-    //negative amount must throw IllegalArgumentException;
-    //a typical implementation will reduce the object's MP by amount ensuring however that MP does not become negative.
+	/**
+     * Negative amount must throw IllegalArgumentException;
+     * a typical implementation will reduce the object's MP by amount ensuring however
+     * that MP does not become negative.
+     * 
+     * @param amount amount of damage done
+     */
+	default void weakenMagic(int amount) {
+		// negative amount must throw IllegalArgumentException;
+		// a typical implementation will reduce the object's MP by amount ensuring however that MP does not become negative.
+		if (amount < 0)
+			throw new IllegalArgumentException("amount must not be negative");
+	}
+    
+	/**
+     * Percentage must be between 0 and 100 (inclusive) otherwise an IllegalArgumentException
+     * must be thrown;
+     * a typical implementation will reduce the object's MP by the percentage given of the
+     * object's basic MP value value ensuring however, that MP does not become negative..
+     * calculations must be done in double data type converting back to int only in the last step
+     * 
+     * @param percentage percentage of damage relative to object's basic MP value
+     */
+	default void weakenMagicPercent(int percentage) {
+	    //percentage must be between 0 and 100 (inclusive) otherwise an IllegalArgumentException must be thrown; 
+	    //a typical implementation will reduce the object's MP by the percentage given of the object's basic MP value value ensuring however, that MP does not become negative..
+	    //calculations must be done in double data type converting back to int only in the last step
+		if (percentage < 0 || percentage > 100)
+			throw new IllegalArgumentException("percentage must be between 0 and 100");
+	}
 
-	default void weakenMagicPercent(int percentage) {}
-    //percentage must be between 0 and 100 (inclusive) otherwise an IllegalArgumentException must be thrown; 
-    //a typical implementation will reduce the object's MP by the percentage given of the object's basic MP value value ensuring however, that MP does not become negative..
-    //calculations must be done in double data type converting back to int only in the last step
+	/**
+	 * Negative amount must throw IllegalArgumentException;
+	 * a typical implementation will increase the object's HP by the amount given.
+	 * 
+     * @param amount amount of healing done
+	 */
+	default void heal(int amount) {
+		//negative amount must throw IllegalArgumentException;
+		//a typical implementation will increase the object's HP by the amount given.
+		if (amount < 0)
+			throw new IllegalArgumentException("amount must not be negative");
+	}
 
-	default void heal(int amount) {}
-   //negative amount must throw IllegalArgumentException;
-   //a typical implementation will increase the object's HP by the amount given.
+	/**
+     * Percentage must be between 0 and 100 (inclusive) otherwise an IllegalArgumentException
+     * must be thrown;
+     * a typical implementation will increase the object's HP by the percentage given of the
+     * object's basic HP value.
+     * calculations must be done in double data type converting back to int only in the last step
+     * 
+     * @param percentage percentage of healing relative to object's basic MP value
+     */
+	default void healPercent(int percentage) {
+	    //percentage must be between 0 and 100 (inclusive) otherwise an IllegalArgumentException must be thrown; 
+	    //a typical implementation will increase the object's HP by the percentage given of the object's basic HP value.
+	    //calculations must be done in double data type converting back to int only in the last step
+		if (percentage < 0 || percentage > 100)
+			throw new IllegalArgumentException("percentage must be between 0 and 100");
+	}
 
-	default void healPercent(int percentage) {}
-    //percentage must be between 0 and 100 (inclusive) otherwise an IllegalArgumentException must be thrown; 
-    //a typical implementation will increase the object's HP by the percentage given of the object's basic HP value.
-    //calculations must be done in double data type converting back to int only in the last step
+	/**
+     * Negative amount must throw IllegalArgumentException;
+     * a typical implementation will increase the object's MP by the amount given.
+     * 
+     * @param amount amount to increase mana
+     */
+	default void enforceMagic(int amount) {
+	    //negative amount must throw IllegalArgumentException;
+	    //a typical implementation will increase the object's MP by the amount given.
+		if (amount < 0)
+			throw new IllegalArgumentException("amount must not be negative");
+	}
 
-	default void enforceMagic(int amount) {}
-    //negative amount must throw IllegalArgumentException;
-    //a typical implementation will increase the object's MP by the amount given.
+	/**
+     * Percentage must be between 0 and 100 (inclusive) otherwise an IllegalArgumentException
+     * must be thrown;
+     * a typical implementation will increase the object's MP by the percentage given of the
+     * object's basic MP value
+     * calculations must be done in double data type converting back to int only in the last
+     * step
+     * 
+     * @param percentage percentage of mana increase relative to object's basic MP value
+     */
+	default void enforceMagicPercent(int percentage) {
+	    //percentage must be between 0 and 100 (inclusive) otherwise an IllegalArgumentException must be thrown; 
+	    //a typical implementation will increase the object's MP by the percentage given of the object's basic MP value
+	    //calculations must be done in double data type converting back to int only in the last step
+		if (percentage < 0 || percentage > 100)
+			throw new IllegalArgumentException("percentage must be between 0 and 100");
+	}
 
-	default void enforceMagicPercent(int percentage) {}
-    //percentage must be between 0 and 100 (inclusive) otherwise an IllegalArgumentException must be thrown; 
-    //a typical implementation will increase the object's MP by the percentage given of the object's basic MP value
-    //calculations must be done in double data type converting back to int only in the last step
-
-	default boolean isProtected(Spell s) {return false;}
-    //if s is null an IllegalArgumentException must be thrown;
-    //an implementation returns true if the object is protected against the spell s, false otherwise.
-
-	default void setProtection(Set<AttackingSpell> attacks) {}
-    //if attacks is null an IllegalArgumentException must be thrown;
-    //a typical implementation will register the object as protected against all the spells in attacks
-
-	default void removeProtection(Set<AttackingSpell> attacks) {}
-    //if attacks is null an IllegalArgumentException must be thrown;
-    //a typical implementation will register the object as not protected against all the spells in attacks
+	/**
+     * If s is null, an IllegalArgumentException must be thrown;
+     * an implementation returns true if the object is protected against the spell s, false
+     * otherwise.
+     * 
+     * @param s spell to test for 
+     * @return true if object is protected against spell s
+     */
+	default boolean isProtected(Spell s) {
+		//if s is null an IllegalArgumentException must be thrown;
+	    //an implementation returns true if the object is protected against the spell s, false otherwise.
+		if (s == null)
+			throw new IllegalArgumentException("spell cannot be null");
+		return false;
+	}
+    
+	/**
+     * If attacks is null an IllegalArgumentException must be thrown;
+     * a typical implementation will register the object as protected against all the spells
+     * in attacks
+     * 
+     * @param attacks list of attacking spells that the object gets protected against
+     */
+	default void setProtection(Set<AttackingSpell> attacks) {
+		//if attacks is null an IllegalArgumentException must be thrown;
+	    //a typical implementation will register the object as protected against all the spells in attacks
+		if (attacks == null)
+			throw new IllegalArgumentException("attacks cannot be null");
+	}
+    
+	/**
+     * If attacks is null an IllegalArgumentException must be thrown;
+     * a typical implementation will register the object as not protected against all the spells
+     * in attacks
+     * 
+     * @param attacks list of attacking spells that the object loses protection against
+     */
+	default void removeProtection(Set<AttackingSpell> attacks) {
+		//if attacks is null an IllegalArgumentException must be thrown;
+	    //a typical implementation will register the object as not protected against all the spells in attacks
+		if (attacks == null)
+			throw new IllegalArgumentException("attacks cannot be null");
+	}
 }
