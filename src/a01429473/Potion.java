8ba0d4f1ec4ad89418fc4748dbcd55914cb74bac
yernaz-togizbayev <yernaz.togizbayev@gmail.com>
2023-06-03 16:29:21 +0200

    implement Potion class

--- src/a01429473/Potion.java
+++ src/a01429473/Potion.java
@@ -1,12 +1,29 @@
 package a01429473;
 
+/**
+ *  A potion has a specific magic effect on its consumer
+ */
 public abstract class Potion extends MagicItem {
-	public void drink(Wizard drinker) {
-	//delegates to method call useOn(drinker)
+	/**
+	 * @param name name
+	 * @param usages number of usages still left
+	 * @param price price
+	 * @param weight weight
+	 */
+	public Potion(String name, int usages, int price, int weight) {
+		super(name, usages, price, weight);
 	}
-	  
+
+	/**
+	 * Delegates to method call useOn(drinker)
+	 * @param drinker the consumer of the potion
+	 */
+	public void drink(Wizard drinker) { useOn(drinker); }
+	 
+	/**
+	 * returns "gulp" if usages is equal to 1, "gulps" otherwise
+	 * @return "gulp" or "gulps" depending on number of usages left
+	 */
 	@Override
-	public String usageString() {
-	//returns "gulp" if usages is equal to 1, "gulps" otherwise
-	}
+	public String usageString() { return (getUsages() == 1) ? "gulp" : "gulps";	}
 }
