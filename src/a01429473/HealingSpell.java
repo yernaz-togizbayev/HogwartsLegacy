57756f3c52d0bc833b27ac79439cea5bd9ac442c
yernaz-togizbayev <yernaz.togizbayev@gmail.com>
2023-06-04 12:27:39 +0200

    implement HealingSpell class

--- src/a01429473/HealingSpell.java
+++ src/a01429473/HealingSpell.java
@@ -1,18 +1,81 @@
 package a01429473;
 
+/**
+ * Healing spells increase the targets health or mana
+ */
 public class HealingSpell extends Spell {
+	/**
+	 * Defines, if HP or MP is affected
+	 * <p>
+	 * type == true: affects HP
+	 * <p>
+	 * type == false: affects MP
+	 */
 	private boolean type;
+	
+	/**
+	 * Defines, if amount is interpreted as an absolute value or as a percentage.
+	 * <p>
+	 * percentage == true: deduct value to subtract as 'amount' percentage of basic value
+	 * <p>
+	 * percentage == false: subtract amount directly 
+	 */
 	private boolean percentage;
-	private int amount; //has to be non negative; if percentage==true, amount must be in the interval [0,100]
-
-	@Override
-	public void doEffect(MagicEffectRealization target) {
-	//use one of the functions heal, healPercent, enforceMagic or enforceMagicPercent according to the flags type and percentage
+	
+	/**
+	 * Has to be non negative; if percentage==true, amount must be in the interval [0,100]
+	 */
+	private int amount;
+	
+	/**
+	 * @param name name
+	 * @param manaCost manaCost
+	 * @param levelNeeded levelNeeded
+	 * @param type defines if health or mana is affected
+	 * @param percentage defines if amount is an absolute or a percentage value
+	 * @param amount amount
+	 */
+	public HealingSpell(String name, int manaCost, MagicLevel levelNeeded, boolean type, boolean percentage, int amount) {
+		super(name, manaCost, levelNeeded);
+		
+		if (amount < 0)
+			throw new IllegalArgumentException("amount has to be non negative");
+		
+		if (percentage && (amount < 0 || amount > 100))
+			throw new IllegalArgumentException("amount must be in the interval [0,100]");
+		
+		this.type = type;
+		this.percentage = percentage;
+		this.amount = amount;
 	}
 
+	/**
+	 * Use one of the functions heal, healPercent, enforceMagic or enforceMagicPercent according
+	 * to the flags type and percentage
+	 * @param target target that receives healing
+	 */
+	@Override
+	public void doEffect(MagicEffectRealization target) {
+		if (type) {
+			if (percentage) target.healPercent(amount);
+			else target.heal(amount);
+		}
+		else {
+			if (percentage) target.enforceMagicPercent(amount);
+			else target.enforceMagic(amount);
+		}
+	}
+
+	/**
+	 * Returns "; +'amount' 'percentage' 'HPorMP'", where 'percentage' is a '%'-sign if percentage
+	 * is true, empty otherwise and HPorMP is HP if type is true, MP otherwise
+	 * e. g. "; +10 HP" or "; +50 % MP"
+	 * @return "; +'amount' 'percentage' 'HPorMP'"
+	 */
 	@Override
 	public String additionalOutputString() {
-	//returns "; +amount 'percentage' 'HPorMP'", where 'percentage' is a '%'-sign if percentage is true, empty otherwise and HPorMP is HP if type is true, MP otherwise
-	//e. g. "; +10 HP" or "; +50 % MP"
+		//returns "; +amount 'percentage' 'HPorMP'", where 'percentage' is a '%'-sign if percentage is true, empty otherwise and HPorMP is HP if type is true, MP otherwise
+		//e. g. "; +10 HP" or "; +50 % MP"
+		return "; +" + amount + (percentage ? " % " : " ") + (type ? "HP" : "MP");
 	}
 }
